239bde000d757c9c127bb445fe6d5edd9743fed5
whatminjacodes <whatminjacodes@gmail.com>
2021-01-27 09:28:37 +0200

    added data binding and click events

--- app/src/main/java/com/minjee/stretchingapp/view/MainFragment.kt
+++ app/src/main/java/com/minjee/stretchingapp/view/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.minjee.stretchingapp.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.minjee.stretchingapp.databinding.FragmentMainBinding
+import com.minjee.stretchingapp.model.StretchMove
 import com.minjee.stretchingapp.viewmodel.MainViewModel
 
 /*
@@ -15,13 +17,10 @@ import com.minjee.stretchingapp.viewmodel.MainViewModel
  *      - shows the UI
  *      - listens to viewModel for updates on UI
  */
-class MainFragment: Fragment() {
+class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
-
-    // Create a viewModel
     private val viewModel: MainViewModel by activityViewModels()
-
     private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
@@ -30,39 +29,43 @@ class MainFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(layoutInflater, container, false)
-        //binding.imageAddFood.setOnClickListener {
-          //  println("The CLICK###")
-        //}
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Start setups
-        setupClickListeners()
         viewModel.initializeListOfMoves()
+        binding.listener = ClickHandler()
 
-        // Recyclerview setup
         linearLayoutManager = LinearLayoutManager(context)
         binding.apply {
             recyclerView.layoutManager = linearLayoutManager
-            recyclerView.adapter = RecyclerViewAdapter(viewModel.getListOfMoves())
+            recyclerView.adapter =
+                RecyclerViewAdapter(viewModel.getListOfMoves()) { stretchMove: StretchMove ->
+                    moveListItemClicked(
+                        stretchMove
+                    )
+                }
             invalidateAll()
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    private fun moveListItemClicked(stretchMove: StretchMove) {
+        Log.d("main fragment: ", "CLICK!")
+        binding.blockClicksView.visibility = View.VISIBLE
+        binding.cardParent.visibility = View.VISIBLE
+        binding.cardMoveName.text = stretchMove.nameOfTheMove
+        binding.cardMoveDescription.text = stretchMove.descriptionOfTheMove
+
     }
 
-    // Setup the button in our fragment to call getUpdatedText method in viewModel
-    private fun setupClickListeners() {
-
-    }
-
-    // Observer is waiting for viewModel to update our UI
-    private fun fragmentTextUpdateObserver() {
-
+    inner class ClickHandler {
+        fun onCardCloseButtonClicked(view: View) {
+            Log.d("main fragment", "close button CLICK")
+            binding.cardParent.visibility = View.GONE
+            binding.blockClicksView.visibility = View.GONE
+        }
     }
 }
+
--- app/src/main/java/com/minjee/stretchingapp/view/RecyclerViewAdapter.kt
+++ app/src/main/java/com/minjee/stretchingapp/view/RecyclerViewAdapter.kt
@@ -1,33 +1,29 @@
 package com.minjee.stretchingapp.view
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.minjee.stretchingapp.databinding.RecyclerviewItemRowBinding
 import com.minjee.stretchingapp.model.ListData
+import com.minjee.stretchingapp.model.StretchMove
 
-class RecyclerViewAdapter(private val items: ListData) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerViewAdapter(private val items: ListData, private val clickListener: (StretchMove) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): StretchMoveHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = RecyclerviewItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return StretchMoveHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        // TODO examine warning "java.lang.IllegalStateException: Handler (rpc) {7d01ae6} sending message to a Handler on a dead thread"
-        holder as StretchMoveHolder
-        holder.viewBinding.itemName.text = items.listOfMoves[position].nameOfTheMove
-        holder.itemView.setOnClickListener {
-            Log.d("Recyclerview", "CLICK!")
-
-        }
+        (holder as StretchMoveHolder).bind(items.listOfMoves[position], clickListener)
     }
 
     override fun getItemCount() = items.listOfMoves.size
 
-    inner class StretchMoveHolder(var viewBinding: RecyclerviewItemRowBinding) : RecyclerView.ViewHolder(viewBinding.root)
+    inner class StretchMoveHolder(private val binding: RecyclerviewItemRowBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(move: StretchMove, clickListener: (StretchMove) -> Unit) {
+            binding.itemName.text = move.nameOfTheMove
+            binding.root.setOnClickListener { clickListener(move) }
+        }
+    }
 }
